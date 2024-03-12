6fffb55402a3f26242f21986920709be6d63613a
nandnexx <84386832+nandnexx@users.noreply.github.com>
2024-03-12 20:11:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapios.bebida import Bebida
+from modelo
+
+restaurante_praca = Restaurante("praça", "Gourmet")
+bebida_suco = Bebida ("Suco de melencia" 5.0, "grande")
+prato_paozinho = Prato ("Paozinho", 2,00, "O melhor pão da cidade")
+
+def main():
+    print(bebida_suco)
+    print(prato_paozinho)
+
+if __name__ == '__main__':
+    main()
+    
+    
+    junior - junior (2,000)
+    junior - Pleno (2,500)
+    junior -0 senior 3,000
+    
+    pleno - junior
+    pleno - pleno
